b282a1e4367a229613574e898758e1f7cf76698f
leonardoschluter <leonardoschluter@gmail.com>
2021-12-06 18:18:37 -0300

    Create android project for SmartDrive

---
+++ smart_drive/app/src/main/java/org/schluter/smartdrive/MainActivity.kt
@@ -0,0 +1,38 @@
+package org.schluter.smartdrive
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import org.schluter.smartdrive.ui.theme.SmartDriveTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SmartDriveTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    SmartDriveTheme {
+        Greeting("Android")
+    }
+}
